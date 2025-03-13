fa6af18134e29b60772ac028a578ee46233c8ca1
angelgandarias <angel.gandarias@opendeusto.es>
2025-03-13 09:50:43 +0100

    Algunos planetas y rotacion sin elipse

---
+++ Assets/Scripts/SolarSystemScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SolarSystemScript : MonoBehaviour
+{
+    public GameObject parentObject;
+    public GameObject childObject;
+    public int rotationSpeed;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        childObject.transform.SetParent(parentObject.transform); 
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        childObject.transform.Rotate(Vector3.left * rotationSpeed * Time.deltaTime);
+        childObject.transform.Rotate(0, 0.1f, 0, Space.Self);
+        //moon.transform.Rotate(0, 0.1f, 0, Space.World);
+
+    }
+}
